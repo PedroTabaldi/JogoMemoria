b67d0b93db8309579949fbc10cd098582131ddbc
PedroTabaldi <pedroaotabaldi@gmail.com>
2025-11-14 21:02:39 -0300

    a

--- JogoMemoria/JogoMemoria/MainWindow.xaml.cs
+++ JogoMemoria/JogoMemoria/MainWindow.xaml.cs
@@ -16,9 +16,28 @@ namespace JogoMemoria
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<BitmapImage> imagens = new List<BitmapImage>();
         public MainWindow()
         {
             InitializeComponent();
+
+            //ramdom rnd = new ramdom();
+
+
+            imagens.Add(new BitmapImage(new System.Uri("JogoMemoria\\img\\comunismo")));
+
+            var img1 = new BitmapImage(new System.Uri("pack://application:,,,/Images/csharp.png"));
+            var img2 = new BitmapImage(new System.Uri("pack://application:,,,/Images/java.png"));
+            var img3 = new BitmapImage(new System.Uri("pack://application:,,,/Images/python.png"));
+            var img4 = new BitmapImage(new System.Uri("pack://application:,,,/Images/javascript.png"));
+            var img5 = new BitmapImage(new System.Uri("pack://application:,,,/Images/ruby.png"));
+            var img6 = new BitmapImage(new System.Uri("pack://application:,,,/Images/swift.png"));
+        }
+
+        private void bt_1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
