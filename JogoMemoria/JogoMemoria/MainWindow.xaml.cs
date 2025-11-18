c1dc02fafab1e3ebdbffe4f8cacfad134e2f6c60
PedroTabaldi <pedroaotabaldi@gmail.com>
2025-11-17 21:46:01 -0300

    a

--- JogoMemoria/JogoMemoria/MainWindow.xaml.cs
+++ JogoMemoria/JogoMemoria/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Security.RightsManagement;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,6 +20,9 @@ namespace JogoMemoria
     {
 
         public List<BitmapImage> imagens = new List<BitmapImage>();
+        public List<Button> btns = new List<Button>();
+
+        public List<string> Escolhas = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
@@ -25,19 +30,58 @@ namespace JogoMemoria
             //ramdom rnd = new ramdom();
 
 
-            imagens.Add(new BitmapImage(new System.Uri("JogoMemoria\\img\\comunismo")));
+            btns.Add(bt_1);
+            btns.Add(bt_2);
+            btns.Add(bt_3);
+            btns.Add(bt_4);
+            btns.Add(bt_5);
+            btns.Add(bt_6);
+            btns.Add(bt_7);
+            btns.Add(bt_8);
+            btns.Add(bt_9);
+            btns.Add(bt_10);
+            btns.Add(bt_11);
+            btns.Add(bt_12);
 
-            var img1 = new BitmapImage(new System.Uri("pack://application:,,,/Images/csharp.png"));
-            var img2 = new BitmapImage(new System.Uri("pack://application:,,,/Images/java.png"));
-            var img3 = new BitmapImage(new System.Uri("pack://application:,,,/Images/python.png"));
-            var img4 = new BitmapImage(new System.Uri("pack://application:,,,/Images/javascript.png"));
-            var img5 = new BitmapImage(new System.Uri("pack://application:,,,/Images/ruby.png"));
-            var img6 = new BitmapImage(new System.Uri("pack://application:,,,/Images/swift.png"));
+
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\comunismo.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\comunismo1.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\cara.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\cara1.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\bozo.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\bozo1.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\tim.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\tim1.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\maca.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\maca1.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\zeca.jpg")));
+            imagens.Add(new BitmapImage(new System.Uri("C:\\Users\\PEDROAUGUSTOPEDROSOT\\Documents\\Projetos em aulas\\UltimoProjeto\\JogoMemoria\\JogoMemoria\\img\\zeca1.jpg")));
+
+
+            Random rnd = new Random();
+            imagens = imagens.OrderBy(x => rnd.Next()).ToList();
+
+            for (int i = 0; i < btns.Count; i++)
+            {
+                var img = new BitmapImage(imagens[i].UriSource);
+                btns[i].Content = new Image { Source = img };
+
+            }
+
         }
 
         private void bt_1_Click(object sender, RoutedEventArgs e)
         {
+            
+        }
+
+        private void bt_2_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+
+
+
+        //bt2
     }
 }
